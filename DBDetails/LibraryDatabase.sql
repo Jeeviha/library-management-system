28898df4786c3a51aa37385f60d6d4805a853906
MJeeviha <MJeeviha@INCH541034.corp.mastechinfotrellis.com>
2025-07-17 16:31:27 +0530

    First Commit

--- DBDetails/LibraryDatabase.sql
+++ DBDetails/LibraryDatabase.sql
@@ -32,7 +32,7 @@ CREATE TABLE `book` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `book`
+-- Dumping.... data for table `book`
 --
 
 LOCK TABLES `book` WRITE;
